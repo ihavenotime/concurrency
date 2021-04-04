d9472c8884f76127218e02c25bf8aac82cfce6c1
Anatoly V <ntnosteam@gmail.com>
2021-04-04 22:27:49 +0300

    Add threading version

--- io_threading.py
+++ io_threading.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python3
 
+import concurrent.futures
 import requests
 import time
+import threading
 
+thread_local = threading.local()
+
+def get_session():
+    if not hasattr(thread_local, "session"):
+        thread_local.session = requests.Session()
+    return thread_local.session
 
 def download_site(url, session):
+    session = get_session()
     with session.get(url) as response:
         print(f"Read {len(response.content)} from {url}")
 
-
 def download_all_sites(sites):
-    with requests.Session() as session:
-        for url in sites:
-            download_site(url, session)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        executor.map(download_site, sites)
 
 
 if __name__ == "__main__":
